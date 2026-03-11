80a372204ede11527390cfe76b73588273b91c07
hackerspider09 <hackerspider09@gmail.com>
2026-03-11 19:57:57 +0530

    if condition added for deplo

--- check.sh
+++ check.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-if (nginx --version >/dev/null 2>&1 ); then
-    echo "Docker is present"
-else
-    echo "Installing docker...."
+# if (nginx --version >/dev/null 2>&1 ); then
+#     echo "Docker is present"
+# else
+#     echo "Installing docker...."
+# fi
+
+# if docker info | grep -q "hackerspider09" || true; then
+#     echo $?
+#     echo "some"
+# else
+#     echo $?
+#     echo "Sdf"
+# fi
+
+if [[ 1 -eq 1 ]]; then
+echo "SDf"
 fi
